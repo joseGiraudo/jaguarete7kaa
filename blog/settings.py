82c28a1f816af97b6b8bde92920b8eef6feb94e2
Jose Giraudo <josegiraudo7@gmail.com>
2021-07-07 20:25:28 -0300

    xd

--- blog/settings.py
+++ blog/settings.py
@@ -144,3 +144,6 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+import django_on_heroku
+django_on_heroku.settings(locals())
